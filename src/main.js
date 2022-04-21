152d2434550cbd5d20bb83f43f8a71f8a4f7cf8c
Grace Joh <graceeunaejoh@gmail.com>
2022-04-20 19:09:29 -0700

    modified main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://blog-api.herokuapp.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://shrouded-shore-78246.herokuapp.com/posts";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
